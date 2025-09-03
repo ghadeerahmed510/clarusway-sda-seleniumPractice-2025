5ea475de6fe3352455b23dcffb77db77b6162ffa
ghadeerahmed510 <ghadeerahmed343@gmail.com>
2025-09-03 08:54:06 +0300

    initial commit

---
+++ src/main/java/com/github/ghadeerahmed510/Main.java
@@ -0,0 +1,13 @@
+package com.github.ghadeerahmed510;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://www.google.com");
+    }
+}
